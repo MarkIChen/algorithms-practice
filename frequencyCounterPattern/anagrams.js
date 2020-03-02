e33a12543f005d6bdb34f64d614eba100f20155f
MarkStudio <markchen8024@gmail.com>
2020-03-02 10:25:07 +0800

    Implement anagram algorithms in JS.

---
+++ frequencyCounterPattern/anagrams.js
@@ -0,0 +1,40 @@
+/*jshint esversion: 6 */
+
+function validAnagram(a, b) {
+  let firstCharArray = a.split('');
+  let secondCharArray = b.split('');
+
+  var firstCollector = {};
+  for (let i = 0; i < firstCharArray.length; i++) {
+    let key = firstCharArray[i];
+    if (!(key in firstCollector)) {
+      firstCollector[key] = 1;
+    } else {
+      firstCollector[key] += 1;
+    }
+  }
+
+  var secondCollector = {};
+  for (let i = 0; i < secondCharArray.length; i++) {
+    let key = secondCharArray[i];
+    if (!(key in secondCollector)) {
+      secondCollector[key] = 1;
+    } else {
+      secondCollector[key] += 1;
+    }
+  }
+
+  for (let i = 0; i < firstCharArray.length; i++) {
+    let key = firstCharArray[i];
+    if (firstCollector[key] != secondCollector[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let a = 'qwetry';
+let b = 'qeywrt';
+
+console.log(validAnagram(a, b));
