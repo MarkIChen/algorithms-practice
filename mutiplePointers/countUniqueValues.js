98563e5f3dca4b082222cff6e3393643f77955dd
MarkStudio <markchen8024@gmail.com>
2020-03-04 10:04:42 +0800

    RAdding comment.

--- mutiplePointers/countUniqueValues.js
+++ mutiplePointers/countUniqueValues.js
@@ -2,13 +2,16 @@
 
 function countUniqueValues(array){
     if(array.length === 0)    return 0;
+    // To scrutinize the whole array to find the unique number.
     let piernierPointer = 0;
+    // To store the unique nunber in the same array.
     let basePointer = 0;
 
     while(piernierPointer < array.length) {
         if(array[piernierPointer] == array[basePointer]) {
             piernierPointer ++;
         } else {
+            // base pointer has to be added before inserting into the array.
             basePointer ++;
             array[basePointer] = array[piernierPointer];
             piernierPointer ++;
@@ -16,8 +19,6 @@ function countUniqueValues(array){
         console.log(array);
 
     }
-
-
     return basePointer + 1;
 }
 
