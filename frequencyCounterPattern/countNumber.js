fc7b68d7862a977effd2a8e03bd4f175cdf0cc0e
MarkStudio <markchen8024@gmail.com>
2020-03-08 11:43:23 +0800

    clean the code

--- frequencyCounterPattern/countNumber.js
+++ frequencyCounterPattern/countNumber.js
@@ -1,30 +1,25 @@
 function sameFrequency(numberA, numberB) {
+  if(numberA.length !== numberB.length)  return false;
 
   var temp = parseInt(numberA %10);
   numberA = parseInt(numberA / 10);
   var collector = {};
-
   // recognize numbers in A
   while(temp) {
-    if (!collector[temp]) {
-      collector[temp] = 1;
-    } else {
-      collector[temp] ++;
-    }
+    collector[temp] = (collector[temp] ? collector[temp] + 1: 1);
 
     temp = parseInt(numberA %10);
     numberA = parseInt(numberA / 10);
   }
-
   // compare to B
   temp = parseInt(numberB % 10);
   numberB = parseInt(numberB / 10);
   while(temp) {
     if(! collector[temp]) {
       return false;
-    } else {
-      collector[temp] -= 1;
     }
+
+    collector[temp] -= 1;
     temp = parseInt(numberB % 10);
     numberB = parseInt(numberB / 10);
   }
