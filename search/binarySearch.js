74c3eff854f36e92d3ad8c8d713f33f34cc6985f
MarkStudio <markchen8024@gmail.com>
2020-03-14 11:34:45 +0800

    recify the algo.

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,23 +1,20 @@
 
 function binarySearch(arr, target) {
-  if(arr.length === 1){
-    return (arr[0] === target)? 0 : -1;
-  }
+  if(arr.length === 1) return (arr[0] === target)? 0 : -1;
 
   var leftPointer = 0;
   var rightPointer = arr.length-1;
 
-  while(leftPointer < rightPointer) {
+  while(leftPointer <= rightPointer) {
     var middle = Math.round((leftPointer + rightPointer) / 2);
-
 
     if(arr[middle] === target) return middle;
 
-    if(arr[middle] > target) rightPointer = middle;
-    else leftPointer = middle;
+    if(arr[middle] > target) rightPointer = middle - 1;
+    else leftPointer = middle + 1;
   }
 
   return -1;
 }
 
-console.log(binarySearch([1, 2, 2, 3, 4, 7, 8, 9], 3));
+console.log(binarySearch([1, 2, 2, 3, 4, 7, 8, 9], 8));
