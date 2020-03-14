564fce983db363a52b0ef6694c164c08084300c9
MarkStudio <markchen8024@gmail.com>
2020-03-14 11:22:22 +0800

    adding binary search.

---
+++ search/binarySearch.js
@@ -0,0 +1,23 @@
+
+function binarySearch(arr, target) {
+  if(arr.length === 1){
+    return (arr[0] === target)? 0 : -1;
+  }
+
+  var leftPointer = 0;
+  var rightPointer = arr.length-1;
+
+  while(leftPointer < rightPointer) {
+    var middle = Math.round((leftPointer + rightPointer) / 2);
+
+
+    if(arr[middle] === target) return middle;
+
+    if(arr[middle] > target) rightPointer = middle;
+    else leftPointer = middle;
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 2, 3, 4, 7, 8, 9], 3));
