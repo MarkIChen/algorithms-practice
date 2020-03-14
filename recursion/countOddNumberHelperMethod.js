1f8b3746c157c933377f30756db85da2d10d9154
MarkStudio <markchen8024@gmail.com>
2020-03-14 10:29:30 +0800

    Integrating file.

--- recursion/countOddNumberHelperMethod.js
+++ recursion/countOddNumberHelperMethod.js
@@ -15,4 +15,18 @@ function countOdd(array) {
   return collectList;
 }
 
+function countOddPure(array){
+  // using devide and conqure pattern
+  collectList = [];
+
+  var target = array[0];
+  if(target % 2 === 1) {
+    collectList.push(target);
+  }
+
+  if(array.length >= 1)
+    collectList = collectList.concat(countOdd(array.slice(1)));
+  return collectList;
+}
+
 console.log(countOdd([1, 2, 3, 4, 5]));
