f1955923c86465b6861d5c0560f4d2327bb9b994
MarkStudio <markchen8024@gmail.com>
2020-03-14 10:25:06 +0800

    Modifing algo.

--- recursion/isPalindrome.js
+++ recursion/isPalindrome.js
@@ -1,10 +1,14 @@
 function isPalindrome(string) {
-  if(string.charAt(0) !== string.charAt(string.length - 1)) {
+
+  if(string.length == 1) {
+    return true;
+  }
+
+  if(string[0] !== string.slice(-1)) {
     return false;
   }
 
-  isPalindrome(string.substr(1, string.length-1));
-  return true;
+  return isPalindrome(string.slice(1, -1));
 }
 
 console.log(isPalindrome("tacocat"));
