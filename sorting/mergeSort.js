7787273c5bb9cfe16709f2148b3c1f9f2f20bc97
MarkStudio <markchen8024@gmail.com>
2020-03-23 11:54:33 +0800

    Improving the efficency of algo.

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -5,7 +5,7 @@ function merge(arr1, arr2) {
   let rightPointer = 0;
 
   let res = [];
-  while(leftPointer < arr1.length || rightPointer < arr2.length) {
+  while(leftPointer < arr1.length && rightPointer < arr2.length) {
     if(arr1[leftPointer] < arr2[rightPointer]){
       res.push(arr1[leftPointer]);
       leftPointer ++;
@@ -14,8 +14,18 @@ function merge(arr1, arr2) {
       rightPointer ++;
     }
   }
+  while(leftPointer < arr1.length) {
+    res.push(arr1[leftPointer]);
+    leftPointer++;
+  }
+  while(rightPointer < arr2.length) {
+    res.push(arr2[rightPointer]);
+    rightPointer ++;
+  }
+
+
   return res;
 }
 
-let res = merge([1, 10, 55],[2, 14, 99, 100]);
+let res = merge([1, 10, 55],[2, 14, 50, 100]);
 console.log(res);
