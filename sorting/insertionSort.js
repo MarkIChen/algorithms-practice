e26614f36a8b1d1059142d2e5805219d33d34a96
MarkStudio <markchen8024@gmail.com>
2020-03-23 10:48:43 +0800

    altering serting sequence of array to improve the algo's efficency.

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -4,18 +4,16 @@ function insertionSort(arr) {
 
   for(let i = 1; i < arr.length; i++) {
     // sorting
-    let index = 0;
-    while(index < i) {
-      if(arr[i] < arr[index]) {
-        // swap
-        let target = arr[i];
-        arr.splice(i, 1);
-        arr.splice(index, 0, target);
-      }
-      console.log(arr);
-      index++;
-    }
+    let index = i;
+    for(let j = i - 1; j >= 0 && arr[i] < arr[j];j--) {
+      index = j;
 
+    }
+    if(index != i){
+      let target = arr[i];
+      arr.splice(i, 1);
+      arr.splice(index, 0, target);
+    }
   }
 
 }
