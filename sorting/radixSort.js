ba6325c9d39e5e32eaf0221aef1bb8174eaa9a16
MarkStudio <markchen8024@gmail.com>
2020-04-07 11:01:42 +0800

    completing radix sort method.

--- sorting/radixSort.js
+++ sorting/radixSort.js
@@ -17,5 +17,31 @@ function mostDigit(arr) {
   return maxDigit;
 }
 
-console.log(mostDigit([1, 23, 412, 5678]));
-// console.log(getDigit(12345, 0));
+function radixSort(arr) {
+  var mostDigitNum = mostDigit(arr);
+
+  for(let i = 0;i < mostDigitNum; i++) {
+    let collector =[];
+    // initialization
+    for(let p = 0;p<10;p++){
+      collector[p]=[];
+    }
+
+    for(let j=0;j<arr.length;j++) {
+      var digit = getDigit(arr[j], i);
+      collector[digit].push(arr[j]);
+      console.log(collector);
+    }
+    // re-order
+    let newArr = [];
+    for(let j=0; j < collector.length; j++){
+      for(let p=0;p<collector[j].length;p++) {
+        newArr.push(collector[j][p]);
+      }
+    }
+    console.log(newArr);
+    arr = newArr;
+  }
+}
+
+console.log(radixSort([2, 1, 40, 212, 11]));
